3ee484b1ef69f6070c96ecc000186403b2f209f1
AhaD <abuahad35188@gmail.com>
2024-01-03 20:36:14 +0600

    footer section

--- src/Page/Footer/Footer.jsx
+++ src/Page/Footer/Footer.jsx
@@ -1,11 +1,57 @@
-     
+import banner from "../../assets/img/slide-2banner.png"
 
 const Footer = () => {
      return (
-          <div>
-               this is e footer section
+          <section className="w-[1920px] relative">
+               <img src={banner} alt="" />
+                
+               <div className="flex ml-[350px] absolute top-2">
+                    <ul className="bg-white">
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Womens’ & Girls’ Fashion</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Health & Beauty</li>
+                         <li className="text-[12px] font-[400] text-[#F97316] ms-2 p-[4px]">Watches, Bags, Jewellery</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Men's & Boys' Fashion</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Mother & Baby</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Electronics Devices</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">TV & Home Appliances</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Electronic Accessories</li>    
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Groceries</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Sports & Outdoors</li>  
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Home & Lifestyle</li> 
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Automobile </li> 
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Computer and laptop</li>                 
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Pet Supplies</li>
+
+                    </ul>
+                    <ul className="bg-white">
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Womens’ & Girls’ Fashion</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Health & Beauty</li>
+                         <li className="text-[12px] font-[400]  ms-2 p-[4px]">Watches, Bags, Jewellery</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Men's & Boys' Fashion</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Mother & Baby</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Electronics Devices</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">TV & Home Appliances</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Electronic Accessories</li>    
+                         <li className="text-[12px] font-[400] text-[#F97316] ms-2 p-[4px]">Groceries</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Sports & Outdoors</li>  
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Home & Lifestyle</li> 
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Automobile </li> 
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Computer and laptop</li>                 
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Pet Supplies</li>
+
+                    </ul>
+                    <ul className="bg-white">
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Womens’ & Girls’ Fashion</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Health & Beauty</li>
+                         <li className="text-[12px] font-[400] ms-2 p-[4px]">Watches, Bags, Jewellery</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Men's & Boys' Fashion</li>
+                         <li className="text-[12px] font-[400] text-[#F97316] ms-2 p-[4px]">Mother & Baby</li>
+                         <li className="text-[12px] font-[400] text-[#2E2E2E] ms-2 p-[4px]">Electronics Devices</li>
+                    </ul>
+               </div>
                
-          </div>
+                    
+          </section>
      );
 };
 
