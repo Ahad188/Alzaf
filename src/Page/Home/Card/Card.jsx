61e9607df1c1499189c8da3d49c07c6222cbff1a
AhaD <abuahad35188@gmail.com>
2024-01-03 21:11:08 +0600

    card design

--- src/Page/Home/Card/Card.jsx
+++ src/Page/Home/Card/Card.jsx
@@ -1,16 +1,74 @@
-import img1 from '../../../assets/img/card/Flash sell.png'
-import img2 from '../../../assets/img/card/Flash sell (1).png'
-import img3 from '../../../assets/img/card/Flash sell (2).png'
-
+ 
+import photo from '../../../assets/img/card/image 274.jpg'
+import photo2 from '../../../assets/img/card/image 274.jpg'
+import photo1 from '../../../assets/img/card/image 274 (1).jpg'
 const Card = () => {
      return (
           <div className='flex gap-[12px]'>
-                <img src={img1} alt="" />
-                <img src={img2} alt="" />
+                {/* <img src={img1} alt="" /> */}
+
+               
+                    <div className="border border-gray-300">
+                         <img src={photo} alt="" />
+                         <p className='text-[14px] text-[#2E2E2E] font-[400] text-center leading-[130%] my-1'>Soft Newborn Baby Wrap <br /> Blankets Baby Sleeping </p>
+                         <div className='flex gap-4 mt-[10px] ms-[8px] p-3'>
+                              <span className='text-[#F97316] text-[14px] font-bold'>৳ 1,9900</span>
+                              <span className='text-[#5A5A5A]  text-[12px] font-[400]'>  <dt>৳ 300</dt> </span>
+
+                         </div>
+                    </div>
+                    <div className="border border-gray-300">
+                         <img src={photo1} alt="" />
+                         <p className='text-[14px] text-[#2E2E2E] font-[400] text-center leading-[130%] my-1'>Soft Newborn Baby Wrap <br /> Blankets Baby Sleeping </p>
+                         <div className='flex gap-4 mt-[10px] ms-[8px] p-3'>
+                              <span className='text-[#F97316] text-[14px] font-bold'>৳ 1,9900</span>
+                              <span className='text-[#5A5A5A]  text-[12px] font-[400]'>  <dt>৳ 300</dt> </span>
+
+                         </div>
+                    </div>
+                    <div className="border border-gray-300">
+                         <img src={photo2} alt="" />
+                         <p className='text-[14px] text-[#2E2E2E] font-[400] text-center leading-[130%] my-1'>Soft Newborn Baby Wrap <br /> Blankets Baby Sleeping </p>
+                         <div className='flex gap-4 mt-[10px] ms-[8px] p-3'>
+                              <span className='text-[#F97316] text-[14px] font-bold'>৳ 1,9900</span>
+                              <span className='text-[#5A5A5A]  text-[12px] font-[400]'>  <dt>৳ 300</dt> </span>
+
+                         </div>
+                    </div>
+                    <div className="border border-gray-300">
+                         <img src={photo} alt="" />
+                         <p className='text-[14px] text-[#2E2E2E] font-[400] text-center leading-[130%] my-1'>Soft Newborn Baby Wrap <br /> Blankets Baby Sleeping </p>
+                         <div className='flex gap-4 mt-[10px] ms-[8px] p-3'>
+                              <span className='text-[#F97316] text-[14px] font-bold'>৳ 1,9900</span>
+                              <span className='text-[#5A5A5A]  text-[12px] font-[400]'>  <dt>৳ 300</dt> </span>
+
+                         </div>
+                    </div>
+                    <div className="border border-gray-300">
+                         <img src={photo} alt="" />
+                         <p className='text-[14px] text-[#2E2E2E] font-[400] text-center leading-[130%] my-1'>Soft Newborn Baby Wrap <br /> Blankets Baby Sleeping </p>
+                         <div className='flex gap-4 mt-[10px] ms-[8px] p-3'>
+                              <span className='text-[#F97316] text-[14px] font-bold'>৳ 1,9900</span>
+                              <span className='text-[#5A5A5A]  text-[12px] font-[400]'>  <dt>৳ 300</dt> </span>
+
+                         </div>
+                    </div>
+                    <div className="border border-gray-300">
+                         <img src={photo2} alt="" />
+                         <p className='text-[14px] text-[#2E2E2E] font-[400] text-center leading-[130%] my-1'>Soft Newborn Baby Wrap <br /> Blankets Baby Sleeping </p>
+                         <div className='flex gap-4 mt-[10px] ms-[8px] p-3'>
+                              <span className='text-[#F97316] text-[14px] font-bold'>৳ 1,9900</span>
+                              <span className='text-[#5A5A5A]  text-[12px] font-[400]'>  <dt>৳ 300</dt> </span>
+
+                         </div>
+                    </div>
+               
+
+                {/* <img src={img2} alt="" />
                 <img src={img3} alt="" />
                 <img src={img1} alt="" />
                 <img src={img1} alt="" />
-                <img src={img1} alt="" />
+                <img src={img1} alt="" /> */}
           </div>
      );
 };
